cb9eac25b8ae603ff637707cfefa8822371f1871
Atmakendric <bapanmandal86958@gmail.com>
2024-01-06 22:51:18 +0530

    Initial commit

---
+++ Plot Guide/app/src/main/java/com/example/plotguide/Activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.plotguide.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.plotguide.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+    }
+}
